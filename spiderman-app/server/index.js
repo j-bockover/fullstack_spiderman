72c08abf1d5dbe5f329f7dfaa3965d2556781536
Mina <minavu@Minas-MacBook-Pro.local>
2022-02-19 17:41:29 -0800

    Can send test data from Express to React.

--- spiderman-app/server/index.js
+++ spiderman-app/server/index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const app = express();
 const port = 5001;
 const path = require("path");
+const axios = require("axios");
 
 app.use(express.static(path.join(__dirname, "..", "build")));
+
+app.get("/api", (req, res) => {
+  axios
+    .get("https://thronesapi.com/api/v2/Characters")
+    .then((response) => {
+      res.send({ data: response.data });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
--- spiderman-app/src/routes/media.js
+++ spiderman-app/src/routes/media.js
@@ -1,16 +1,18 @@
-import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Image from "../img/spidermanswing.jfif";
-import Image2 from "../img/spideyaction.gif";
-import Image6 from "../img/cbbugle.jpg";
-import Image7 from "../img/cbchar.jpg";
-import Image8 from "../img/cbmedia.jpg";
-import Image9 from "../img/cborigins.jpg";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
+import "../App.css";
 import Navbar from "../components/navbar";
-// import React, { useState, useEffect } from "react";
 
 export default function Media(props) {
+  const [data, setData] = React.useState("");
+
+  axios
+    .get("/api")
+    .then((response) => console.log(response.data))
+    .catch((error) => console.error(error));
+
   return <Navbar />;
 }
