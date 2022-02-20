7dded009717161ab824e709ffe16e90628239a96
Mina <minavu@Minas-MacBook-Pro.local>
2022-02-19 23:02:09 -0800

    Still working on media page two column layout.

--- spiderman-app/server/index.js
+++ spiderman-app/server/index.js
@@ -3,18 +3,14 @@ const app = express();
 const port = 5001;
 const path = require("path");
 const axios = require("axios");
+const { json } = require("express");
+
+let movie_list = getMovies();
 
 app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.get("/api", (req, res) => {
-  axios
-    .get("https://thronesapi.com/api/v2/Characters")
-    .then((response) => {
-      res.send({ data: response.data });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  res.send(movie_list);
 });
 
 app.use((req, res, next) => {
@@ -24,3 +20,105 @@ app.use((req, res, next) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
 });
+
+function getMovies() {
+  let movie_api_key = "5941d4436aff4a93f3f11e86cb336bec";
+  let movie_images_config;
+  let movie_list = [];
+  axios
+    .get(`https://api.themoviedb.org/3/configuration?api_key=${movie_api_key}`)
+    .then((response) => {
+      movie_images_config = { ...response.data.images };
+      getMovieData();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  const getMovieData = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${movie_api_key}&query=Spider-Man`
+      )
+      .then((response) => {
+        let { results } = response.data;
+        results.forEach((result) => {
+          let movie = {
+            id: result.id,
+            title: result.title,
+            overview: result.overview,
+            release_date: result.release_date,
+            image:
+              movie_images_config.secure_base_url +
+              movie_images_config.poster_sizes[3] +
+              result.poster_path,
+          };
+          movie_list.push(movie);
+        });
+        movie_list.sort((a, b) => {
+          let a_date = new Date(a.release_date.split("-"));
+          let b_date = new Date(b.release_date.split("-"));
+          return b_date.getFullYear() - a_date.getFullYear();
+        });
+        console.log(movie_list);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    return;
+  };
+
+  return movie_list;
+  // const getCastCrew = (movie_id) => {
+  //   // let movie_cast = [];
+  //   let movie_crew_deep = [];
+  //   axios
+  //     .get(
+  //       `https://api.themoviedb.org/3//movie/${movie_id}/credits?api_key=${movie_api_key}`
+  //     )
+  //     .then((response) => {
+  //       let { cast, crew } = response.data;
+  //       // movie_cast = cast.slice(0, 10).map((c) => {
+  //       //   return {
+  //       //     id: c.id,
+  //       //     name: c.name,
+  //       //     character: c.character,
+  //       //     profile_path: c.profile_path,
+  //       //     order: c.order,
+  //       //   };
+  //       // });
+  //       // console.log(movie_cast);
+  //       // let movie_crew_shallow = crew
+  //       //   .filter((c) => c.job === "Director" || c.job === "Executive Producer")
+  //       //   .map((c) => {
+  //       //     return {
+  //       //       id: c.id,
+  //       //       name: c.name,
+  //       //       job: c.job,
+  //       //     };
+  //       //   });
+  //       crew.forEach((crew_member) => {
+  //         if (
+  //           crew_member.job === "Director" ||
+  //           crew_member.job === "Executive Producer"
+  //         ) {
+  //           let member = {
+  //             id: crew_member.id,
+  //             name: crew_member.name,
+  //             job: crew_member.job,
+  //           };
+  //           movie_crew_deep.push(member);
+  //         }
+  //       });
+  //       // console.log(movie_crew_deep);
+  //       return movie_crew_deep;
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  //   console.log(movie_crew_deep);
+  //   // return {
+  //   //   movie_cast,
+  //   //   movie_crew,
+  //   // };
+  // };
+}
--- spiderman-app/src/routes/media.js
+++ spiderman-app/src/routes/media.js
@@ -1,18 +1,52 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
 
 import "../App.css";
 import Navbar from "../components/navbar";
+class MovieList extends React.Component {
+  state = {
+    moives: [],
+  };
+
+  componentDidMount() {
+    axios.get(`/api`).then((res) => {
+      const moives = res.data;
+      this.setState({ moives });
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.state.moives.map((movie) => (
+          // <>
+          <Row className="show-grid">
+            <Col>
+              <div className="d-flex">
+                <img src={movie.image} width="auto" height="200px"></img>
+                <div>
+                  <h4>{movie.title}</h4>
+                  <p>{movie.overview}</p>
+                  <p>{movie.release_date}</p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          // </>
+        ))}
+      </Container>
+    );
+  }
+}
 
 export default function Media(props) {
-  const [data, setData] = React.useState("");
-
-  axios
-    .get("/api")
-    .then((response) => console.log(response.data))
-    .catch((error) => console.error(error));
-
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <MovieList />
+    </>
+  );
 }
